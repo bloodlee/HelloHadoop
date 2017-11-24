84b4f0548f043ba2deb7ef2f603200e651a74dfe
Yongqiang Li <jasonleehust@gmail.com>
2017-11-24 15:50:18 -0800

    Init commit.

---
+++ src/org/yli/MaxTemperatureMapper.java
@@ -0,0 +1,18 @@
+package org.yli;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private static final int MISSING = 9999;
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
+}
